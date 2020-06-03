9ea64aa93887495b038b5ef832321aa525f4c199
Oleg <60295799+OlEzhA0@users.noreply.github.com>
2020-06-03 21:10:55 +0300

    Updates

---
+++ precache-manifest.f46dc618f8f8dac95ede84597ea1add0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f00b80d0bd556ea8cc922df8fa49d2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "622a16345e33080c5134",
+    "url": "./static/css/main.0979e4e3.chunk.css"
+  },
+  {
+    "revision": "3c57004b5f0c71c7621f",
+    "url": "./static/js/2.45377ff7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.45377ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622a16345e33080c5134",
+    "url": "./static/js/main.67888e78.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "d9f61e28c0ce1904d508eb52aadcca0e",
+    "url": "./static/media/popupImage.d9f61e28.png"
+  }
+]);
